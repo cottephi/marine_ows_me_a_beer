9cdeda712a18fae3fd7c289272269fa97f608330
Philippe <rickaphilippe@gmail.com>
2021-10-11 23:32:00 +0200

    mca -> 2 axes

--- adisc.R
+++ adisc.R
@@ -17,3 +17,30 @@ data$ALIVE.J30[data$ALIVE.J30=="NA"] <- NA
 
 data <- subset(data, subset = !is.na(data$HEMOGLOBINE_PREOP))
 data <- subset(data, subset = !is.na(data$ALIVE.J30))
+
+# On sélectionne les variables qualitatives a priori intéressantes
+data <- subset(data, select=c(14:28,30:36,38:43,45,48:50,52:56,64,65,68,72:78,80,81,87,81,83,86:89,93,96,98,99,118:135,137))
+
+# On supprime celles qui n'ont qu'un seul niveau ou trop spécifiques
+apply(data, 2, unique)
+data$COMP.ACR <- NULL
+data$NSQ_Cardiac.arrest <- NULL
+data$INHOSPITAL.death <- NULL
+data$DISCHARGE.before.J30 <- NULL
+data$ACS.NSQ_comp <- NULL
+data$Surgical.procedure <- NULL
+
+# Analyse de Correspondances Multiples
+library(DescTools)
+library(FactoMineR)
+library(factoextra)
+
+data_acm <- data[,1:72]
+res.acm <- MCA(data_acm, graph=F, ncp=2) # deux axes => deux critères composites
+
+## On ajoute à chaque individu ses coordonnées sur le plan factoriel
+
+data$x1 <- res.acm$ind$coord[,1]
+data$x2 <- res.acm$ind$coord[,2]
+
+## Il faut maintenant mener une analyse discriminante sur ces coordonnées par rapport à la variable anémie
